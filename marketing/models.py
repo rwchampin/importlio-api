ca718fad414ac9c9f15b378ffe3b11310329061e
Ryan Champin <rwchampin@gmail.com>
2023-09-30 11:32:48 -0400

    updates

--- marketing/models.py
+++ marketing/models.py
@@ -7,73 +7,76 @@ list_level_TYPES = (
     ('3', 'Enterprise'),
 )
 
-class Niche(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
+# slugify mixin that makes a slug field and updates it on every save and uses the models
+# name field as the default field to slugify
+# auto slugify mixin and if no title it names it untitled<num of eisting untitled + 1>
+class AutoSlugMixin(models.Model):
+    slug = models.SlugField( null=True, max_length=255)
+    class Meta:
+        abstract = True
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        else:
+            #amount of existing slugs with the same name + 1
+            self.slug = slugify(self.name) + str(self.__class__.objects.filter(slug__startswith=slugify(self.name)).count() + 1)
+        super(AutoSlugMixin, self).save(*args, **kwargs)
+        
+        
 
 class MarketingStatistic(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    niche = models.ForeignKey(Niche, related_name='statistics', on_delete=models.SET_NULL, null=True, blank=True)
-
+    marketing_list = models.ForeignKey('MarketingList', related_name='statistics', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
-
-class DataSource(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
+ 
 
 class Email(models.Model):
     email = models.EmailField(unique=True, max_length=255)
-    niches = models.ManyToManyField(Niche, related_name='emails')
+    marketing_list = models.ManyToManyField('MarketingList', related_name='emails')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.email
 
-class Tag(models.Model):
+class ListType(AutoSlugMixin, models.Model):
     name = models.CharField(max_length=255, unique=True)
-    niches = models.ManyToManyField(Niche, related_name='tags')
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self):
+        return self.name
+
+
+class Tag(AutoSlugMixin, models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-
-class MarketingList(models.Model):
+class MarketingList(AutoSlugMixin, models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     level = models.CharField(max_length=1, choices=list_level_TYPES, default='1')
-    fake = models.BooleanField(default=False)
     url = models.SlugField(blank=True, null=True, max_length=255)
-    niches = models.ManyToManyField(Niche, related_name='marketing_lists')
-
-    def save(self, *args, **kwargs):
-        self.url = slugify(self.name)
-        super(MarketingList, self).save(*args, **kwargs)
-        # Calculate and update the size of the marketing list
-        self.update_size()
-
-    def update_size(self):
-        # Calculate the total size by summing the sizes of emails for all associated niches
-        total_size = Email.objects.filter(niches__in=self.niches.all()).count()
-        self.marketingliststatistic_set.update_or_create(defaults={'size': total_size})
-
+    emails = models.ManyToManyField(Email, related_name='lists')
+    type = models.ForeignKey(ListType, related_name='lists', on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='lists')
+    
     def __str__(self):
         return self.name
+    
+    
+    
